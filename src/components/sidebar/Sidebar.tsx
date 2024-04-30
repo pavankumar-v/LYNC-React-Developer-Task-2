79e835fdb0191b0efbc478cf7cfaeb171c36dfb6
pavankumar-v <pavankumarv124@gmail.com>
2024-04-30 23:55:58 +0530

    perf(clearnup): remove commented code

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -6,26 +6,6 @@ import { Layout } from 'antd';
 const { Sider } = Layout;
 
 const { Title } = Typography;
-// type MenuItem = Required<MenuProps>['items'][number];
-
-// function getItem(
-//   label: React.ReactNode,
-//   key: React.Key,
-//   icon?: React.ReactNode,
-//   children?: MenuItem[]
-// ): MenuItem {
-//   return {
-//     key,
-//     icon,
-//     children,
-//     label,
-//   } as MenuItem;
-// }
-
-// const items: MenuItem[] = [
-//   getItem('Option 1', '1', <PieChartOutlined />),
-//   getItem('Option 2', '2', <DesktopOutlined />),
-// ];
 
 const Sidebar: React.FC = () => {
   return (
@@ -33,28 +13,7 @@ const Sidebar: React.FC = () => {
       <Title level={5} className="!text-white p-4">
         File Drive
       </Title>
-      <Menu
-        theme="dark"
-        mode="inline"
-        defaultSelectedKeys={['1']}
-        items={[
-          {
-            key: '1',
-            // icon: <UserOutlined />,
-            label: 'nav 1',
-          },
-          {
-            key: '2',
-            // icon: <VideoCameraOutlined />,
-            label: 'nav 2',
-          },
-          {
-            key: '3',
-            // icon: <UploadOutlined />,
-            label: 'nav 3',
-          },
-        ]}
-      />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={[]} />
     </Sider>
   );
 };
